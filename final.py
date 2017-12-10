777398c6a22c4e3fa933bd640dfd3fab9ab14ad0
CodeBloodedChris <33336510+CodeBloodedChris@users.noreply.github.com>
2017-12-10 12:35:57 -0800

    Graphics!!!

--- final.py
+++ final.py
@@ -4,9 +4,11 @@
 #BEWARE OF MONSTERS!
 #Monsters move around the map and will eat the player if the players lands on them or they land on the player.
 import random
+import time
+import java.awt.Font as Font
 
 #GLOBAL VARIABLES
-MAP_SIZE = 5 #Exponential, so MAP_SIZE = 4 would mean 16 squares and so on.
+MAP_SIZE = 7 #Exponential, so MAP_SIZE = 4 would mean 16 squares and so on.
 NUM_MONSTERS = 5 #Monsters that search for the player.
 NUM_SPINNING_ROOMS = 2 #Spinning rooms send player off in random direction!
 NUM_KEYS = 3 #Keys needed to unlock an escape door.
@@ -63,16 +65,16 @@ def getRandomStart(map):
 #Takes player direction and returns true or false if that direction is valid.
 def movePlayer(map, player_pos, direction):
   change = false
-  if direction == 'N' and player_pos['Y'] + 1 < len(map):
+  if direction == 'W' and player_pos['Y'] + 1 < len(map):
     player_pos['Y'] =  player_pos['Y'] + 1
     change = true
   elif direction == 'S' and  player_pos['Y'] - 1 >= 0:
     player_pos['Y'] =  player_pos['Y'] - 1
     change = true
-  elif direction == 'E' and  player_pos['X'] + 1 < len(map):
+  elif direction == 'D' and  player_pos['X'] + 1 < len(map):
     player_pos['X'] =  player_pos['X'] + 1
     change = true
-  elif direction == 'W' and  player_pos['X'] - 1 >= 0:
+  elif direction == 'A' and  player_pos['X'] - 1 >= 0:
     player_pos['X'] =  player_pos['X'] - 1
     change = true
   return change
@@ -115,18 +117,108 @@ def moveMonsters(map):
       if map[x][y] == 'T':
         map[x][y] = 'M'
 
+#Function to grab tile from tileset and copy it into world
+def tileCopy(source, sourceX, sourceY, target, targetX, targetY):
+  new_y = targetY - 1
+  for y in range (sourceY,sourceY+32):
+    if new_y < getHeight(target)-1:
+      new_y = new_y + 1
+      new_x = targetX - 1
+    for x in range (sourceX, sourceX+32):
+      if new_x < getWidth(target)-1:
+        new_x = new_x + 1
+      original=getPixel(source, x, y)
+      new=getPixel(target, new_x, new_y)
+      color=getColor(original)
+      setColor(new, color)
+  return target
+
+def deathColoredGlasses(pic):
+  pixels = getPixels(pic)
+  for p in pixels:
+    b = getBlue(p)
+    setBlue(p, b*.5)
+    g = getGreen(p)
+    setGreen(p, g*.5)
+    r = getRed(p)
+    setRed(p, r*2)
+  return pic
+  
+#Function to color a picture completely black
+def turnBlack(pic):
+  pixels = getPixels(pic)
+  for i in pixels:
+    setBlue(i,0)
+    setGreen(i,0)
+    setRed(i,0)
+  return pic
+
+#Create a blank "world" and turn it black  
+world = makeEmptyPicture(64 + (32*MAP_SIZE), 64 + (32*MAP_SIZE))
+blackCanvas = makeEmptyPicture(32*MAP_SIZE, 32*MAP_SIZE)
+blackCanvas = turnBlack(blackCanvas)
+
+#Open up tile set
+dir = os.path.dirname(__file__)#uses the parent folder of the program
+path = dir + "\\"+"DUNGEON.png"#creates a pathway with the desired file name
+tile = makePicture(path)
+
+#Create stone walls around edge of world
+for x in range (0,MAP_SIZE+2):
+  for y in range (0,2):
+    tileCopy(tile, 448, 64, world, 1+(x*32), 1+(32*(MAP_SIZE+1)*y))
+for x in range (0,MAP_SIZE+1):
+  for y in range (0,2):
+    tileCopy(tile, 448, 64, world, 1+(32*(MAP_SIZE+1)*y) , 1+(32*x))
+show(world)
+
+translate = []
+u = MAP_SIZE-1
+for s in range (MAP_SIZE):
+  translate.append(u) #This translates the y postition of the player for the graphics
+  u -= 1
+  
 #Display the map with a P for player's current position.
 def printMap(map, player):
   current_room = game_map[player['X']][player['Y']]  
   map[player['X']][player['Y']] = 'P'
-  
+  copyInto(blackCanvas, world, 32, 32)#Set the middle of the room black
+    
+    #Add Graphics to room
   for y in range(len(map)):
     for x in range(len(map)):
-      print map[x][len(map) - y - 1],
-    print '' 
-  
+      if map[x][len(map) - y - 1] == 'D':
+        tileCopy(tile, 800, 160, world, 32+(32*x), 32+(32*y))
+      elif map[x][len(map) - y - 1] == 'K':
+        tileCopy(tile, 224, 256, world, 32+(32*x), 32+(32*y))
+      elif map[x][len(map) - y - 1] == 'M':
+        tileCopy(tile, 544, 416, world, 32+(32*x), 32+(32*y))
+      elif map[x][len(map) - y - 1] == 'P':
+        tileCopy(tile, 384, 320, world, 32+(32*x), 32+(32*y))   
+      elif map[x][len(map) - y - 1] == 'S':
+        tileCopy(tile, 256, 256, world, 32+(32*x), 32+(32*y))                 
+                                                        
   map[player['X']][player['Y']] = current_room
+  
+  #Animate the player at the end of move
+  if game_map[player['X']][player['Y']] != 'S':#If he's not in a room spinner, normal animation
+    for x in range(0,3):
+      repaint(world)
+      tileCopy(tile, 384+(32*x), 320, world, 32+(32*player['X']), 32+(32*translate[player['Y']]))
+      time.sleep(.15)
+  else:#Else, give him a spinner animation
+    for x in range(0,3):
+      repaint(world)
+      tileCopy(tile, 384+(32*x), 480, world, 32+(32*player['X']), 32+(32*translate[player['Y']]))
+      time.sleep(.15)
 
+  repaint(world)
+  
+def createText(picture, text, size, x, y, c):
+  myFont = makeStyle("Blackadder ITC", Font.BOLD, size) #you can change the font to anything in java awt
+  if c == 0: addTextWithStyle(picture, x, y, text, myFont, yellow)
+  else: addTextWithStyle(picture, x, y, text, myFont, red)
+  
 #Create the game map.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
 blank_map = createMap(MAP_SIZE)
 features = createFeatureRandomSeed(MAP_SIZE, NUM_MONSTERS, NUM_SPINNING_ROOMS, NUM_KEYS, NUM_DOORS)
@@ -135,10 +227,13 @@ player = getRandomStart(game_map)
 
 #Start the game.
 game_over = false
+death = false
+win = false
 key_count = 0
 printMap(game_map, player)
 while game_over == false:
-  move_direction = raw_input("Enter a direction (Ex. 'N','S','E','W'): ")
+  move_direction = requestString("Enter a direction:\n'W' for up\n'A' for left\n'S' for down\n'D' for right): ")
+  moveMonsters(game_map)
   
   player_moved = false
   if game_map[player['X']][player['Y']] == 'S': #Check for Spinning Room
@@ -149,17 +244,50 @@ while game_over == false:
     player_moved = movePlayer(game_map,player,move_direction.upper())                
   
   if player_moved:
-    moveMonsters(game_map)
     if game_map[player['X']][player['Y']] == 'K': #GET THE KEY
       key_count += 1
       game_map[player['X']][player['Y']] = 'E'
     elif game_map[player['X']][player['Y']] == 'D' and key_count == NUM_KEYS: #WIN THE GAME!
-      printNow("YOU WIN! You escaped the labyrinth!")
+      #printNow("YOU WIN! You escaped the labyrinth!")
       game_over = true
+      win = true
     elif game_map[player['X']][player['Y']] == 'M': #OH NO MONSTER
-      printNow("YOU LOSE! You were eaten by a monster! OH THE HORROR!")
+      #showInformation("YOU LOSE! You were eaten by a monster! OH THE HORROR!")
+      death = true
       game_over = true
+      
     printMap(game_map, player)
+    if death == true and win != true:
+      tileCopy(tile, 320, 128, world, 32+(32*player['X']), 32+(32*translate[player['Y']]))
+      world = deathColoredGlasses(world)
+      repaint(world)
+      time.sleep(1)
+      turnBlack(world)
+      tileCopy(tile, 320, 128, world, 32+(32*player['X']), 32+(32*translate[player['Y']]))
+      createText(world, "You're dead!", 40, 20, 50, 1)
+      repaint(world)
+    if win == true and death != true:
+      turnBlack(world)
+      tileCopy(tile, 384, 320, world, 100, 100)
+      repaint(world)
+      time.sleep(1)
+      y = 0
+      while y < 70:
+        y += 10
+        turnBlack(world)
+        tileCopy(tile, 384, 320, world, 100, 100)
+        tileCopy(tile, 672, 416, world, 100, y)
+        repaint(world)
+      turnBlack(world)
+      tileCopy(tile, 960, 352, world, 100, 100)
+      repaint(world)
+      time.sleep(.5)
+      turnBlack(world)
+      tileCopy(tile, 992, 352, world, 100, 100)
+      createText(world, "You win!", 40, 20, 50, 0)
+      repaint(world)     
+      
+      
   else:
     printNow("Invalid move!")
 
